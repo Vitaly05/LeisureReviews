94fa27591133b07534cc7ec069db1b4b3c759777
Виталий Лозюк <vitalik050405@gmail.com>
2023-08-30 21:58:24 +0300

    Added Tag model to make many-to-many relationship

---
+++ LeisureReviews/Models/Database/Tag.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations;
+
+namespace LeisureReviews.Models.Database
+{
+    [Index(nameof(Name), IsUnique = true)]
+    public class Tag
+    {
+        [Key]
+        public string Id { get; set; }
+
+        [Required]
+        public string Name { get; set; } = "";
+
+        public ICollection<Review> Reviews { get; set; } = new List<Review>();
+    }
+}
